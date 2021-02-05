05ae942f87d571549b6278cfcc8067ba62765f3f
Krzysztof101 <krzysztofgryko@poczta.fm>
2021-02-05 10:10:48 +0100

    user app almost finished

---
+++ RecommendorsSystem/UserControlRegister.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NavigationInterfaces;
+
+namespace RecommendorsSystem
+{
+    public partial class UserControlRegister : UserControl
+    {
+        IRegisterFunctions iRegFunctions;
+        INavRegister iNavRegister;
+        public UserControlRegister(IRegisterFunctions iregfuns, INavRegister inavreg)
+        {
+            InitializeComponent();
+            iNavRegister = inavreg;
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            iNavRegister.goToLogin();
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            if(checkCredentials())
+            {
+
+            }
+            else
+            {  }
+        }
+        private bool checkCredentials()
+        {
+            string user = textBoxUser.Text;
+            string pswd1 = textBoxPassword1.Text;
+            string pswd2 = textBoxPassword2.Text;
+            if(user.Length == 0 || pswd1.Length == 0 || pswd2.Length == 0)
+            {
+                return false;
+            }
+            if(pswd1 != pswd2)
+            {
+                return false;
+            }
+            if(!passwordStrong(pswd))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
